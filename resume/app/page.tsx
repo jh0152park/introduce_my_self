b10f3d87ede18f5556411bd6564dbfc789262f7c
jh0152park <jh0152park@naver.com>
2024-06-21 20:37:50 +0900

    added global background image and deleted style combine function

--- resume/app/page.tsx
+++ resume/app/page.tsx
@@ -2,9 +2,9 @@ import Header from "@/components/navigate/header";
 
 export default function Home() {
     return (
-        <div>
+        <div className="flex items-start justify-center w-full">
             <Header />
-            <main className="p-10">
+            <main className="p-[5%] w-full h-screen max-w-[800px]  ">
                 <span>hi</span>
             </main>
         </div>
--- resume/components/navigate/header.tsx
+++ resume/components/navigate/header.tsx
@@ -1,14 +1,9 @@
 import { POINT_COLOR } from "@/global/style";
-import { styleCombine } from "@/utils/style/style-combine";
 
 export default function Header() {
     return (
         <div
-            className={styleCombine([
-                "w-full h-[10px] bg-gradient-to-r fixed top-0",
-                `from-[${POINT_COLOR.dark}]`,
-                `to-[${POINT_COLOR.light}]`,
-            ])}
+            className={`w-full h-[10px] bg-gradient-to-r fixed top-0 from-[${POINT_COLOR.dark}] to-[${POINT_COLOR.light}]`}
         ></div>
     );
 }
