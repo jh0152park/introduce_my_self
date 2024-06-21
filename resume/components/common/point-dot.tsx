ba1bb99b7cc2f3186e2aaac7d0504218434188ed
jh0152park <jh0152park@naver.com>
2024-06-22 03:00:38 +0900

    added custom colors into tailwind config

--- resume/components/common/point-dot.tsx
+++ resume/components/common/point-dot.tsx
@@ -1,5 +1,3 @@
-import { POINT_COLOR } from "@/global/style";
-
 export default function PointDot() {
-    return <span className={`text-[${POINT_COLOR.dark}]`}>.</span>;
+    return <span className="text-dark">.</span>;
 }
